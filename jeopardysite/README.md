7c08dc9d3524256c254693bdce31769bae39146d
No VBuck :( <48155462+emeraldtip@users.noreply.github.com>
2024-01-17 11:55:49 +0200

    Added jeopardysite for lighting control

---
+++ jeopardysite/README.md
@@ -0,0 +1,8 @@
+#JeopardySite
+
+This is an html document thing that is used for the background lighting of a 5 player jeopardy game.
+
+Lighting setup:
+8x Digital Sputnik voyager 4ft on stands
+Voyager controller app on windows
+OBS2Spout plugin + Spout to pass video to the Voyager controller through OBS
---
+++ jeopardysite/script.js
@@ -0,0 +1,107 @@
+function clearLights() {
+    element = document.getElementById("rect1");
+    element.style.backgroundColor = "rgba(0,0,0,0)";
+    element = document.getElementById("rect2");
+    element.style.backgroundColor = "rgba(0,0,0,0)";
+    element = document.getElementById("rect3");
+    element.style.backgroundColor = "rgba(0,0,0,0)";
+    element = document.getElementById("rect4");
+    element.style.backgroundColor = "rgba(0,0,0,0)";
+    element = document.getElementById("rect5");
+    element.style.backgroundColor = "rgba(0,0,0,0)";
+    element = document.getElementById("rect6");
+    element.style.backgroundColor = "rgba(0,0,0,0)";
+}
+
+function p1c() {
+    clearLights();
+    element = document.getElementById("rect1");
+    element.style.backgroundColor = "lime";
+    
+    element = document.getElementById("rect2");
+    element.style.backgroundColor = "lime";
+}
+
+function p2c() {
+    clearLights();
+    element = document.getElementById("rect2");
+    element.style.backgroundColor = "lime";
+    
+    element = document.getElementById("rect3");
+    element.style.backgroundColor = "lime";
+}
+
+function p3c() {
+    clearLights();
+    element = document.getElementById("rect3");
+    element.style.backgroundColor = "lime";
+    
+    element = document.getElementById("rect4");
+    element.style.backgroundColor = "lime";
+}
+
+function p4c() {
+    clearLights();
+    element = document.getElementById("rect4");
+    element.style.backgroundColor = "lime";
+    
+    element = document.getElementById("rect5");
+    element.style.backgroundColor = "lime";
+}
+
+function p5c() {
+    clearLights();
+    element = document.getElementById("rect5");
+    element.style.backgroundColor = "lime";
+    
+    element = document.getElementById("rect6");
+    element.style.backgroundColor = "lime";
+}
+
+
+
+
+function p1w() {
+    clearLights();
+    element = document.getElementById("rect1");
+    element.style.backgroundColor = "red";
+    
+    element = document.getElementById("rect2");
+    element.style.backgroundColor = "red";
+}
+
+function p2w() {
+    clearLights();
+    element = document.getElementById("rect2");
+    element.style.backgroundColor = "red";
+    
+    element = document.getElementById("rect3");
+    element.style.backgroundColor = "red";
+}
+
+function p3w() {
+    clearLights();
+    element = document.getElementById("rect3");
+    element.style.backgroundColor = "red";
+    
+    element = document.getElementById("rect4");
+    element.style.backgroundColor = "red";
+}
+
+function p4w() {
+    clearLights();
+    element = document.getElementById("rect4");
+    element.style.backgroundColor = "red";
+    
+    element = document.getElementById("rect5");
+    element.style.backgroundColor = "red";
+}
+
+function p5w() {
+    clearLights();
+    element = document.getElementById("rect5");
+    element.style.backgroundColor = "red";
+    
+    element = document.getElementById("rect6");
+    element.style.backgroundColor = "red";
+}
